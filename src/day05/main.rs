f512b14d4e4542244d79391d0a518c791462dd64
Mike Meyer <mike@meyermike.com>
2022-12-05 17:57:58 -0500

    simplifying a bit

--- src/day05/main.rs
+++ src/day05/main.rs
@@ -1,46 +1,40 @@
 use itertools::Itertools;
 use shared::{map_words, puzzle_input};
-use std::collections::VecDeque;
 
-fn parse_stacks(s: &str) -> Vec<VecDeque<char>> {
-    let mut stacks = vec![VecDeque::<char>::new(); 9];
+fn parse_stacks(s: &str) -> Vec<String> {
+    let mut stacks = vec![String::new(); 9];
     for line in s.split('\n') {
         for (i, mut cs) in line.chars().chunks(4).into_iter().enumerate() {
             if let Some(c) = cs.find(|c| c.is_alphabetic()) {
-                stacks.get_mut(i).unwrap().push_front(c);
+                stacks.get_mut(i).unwrap().insert(0, c);
             }
         }
     }
     stacks
 }
 
-fn run<I: Iterator<Item = (usize, usize, usize)>>(
-    stacks: &mut [VecDeque<char>],
-    cmds: I,
-    p2: bool,
-) -> String {
-    for (count, from, to) in cmds {
+fn run(stacks: &mut [String], code: &str, reverse: bool) -> String {
+    for line in code.split('\n') {
+        let (count, from, to) = map_words!(line, |s: &str| s.parse::<usize>().unwrap(), 1, 3, 5);
         let from_v = stacks.get_mut(from - 1).unwrap();
         let to_move = from_v.split_off(from_v.len() - count);
         let to_v = stacks.get_mut(to - 1).unwrap();
-        if p2 {
-            to_v.extend(to_move.iter());
+        if reverse {
+            to_v.extend(to_move.chars().rev());
         } else {
-            to_v.extend(to_move.iter().rev());
+            to_v.extend(to_move.chars());
         }
     }
-    stacks.iter().map(|v| v.back().unwrap()).collect::<String>()
+    stacks
+        .iter_mut()
+        .map(|v| v.pop().unwrap())
+        .collect::<String>()
 }
 
 pub fn main() {
     let input = puzzle_input!();
-    let mut halves = input.split("\n\n");
-    let mut stacks = parse_stacks(halves.next().unwrap());
-    let cmds = halves
-        .next()
-        .unwrap()
-        .split('\n')
-        .map(|line| map_words!(line, |s: &str| s.parse::<usize>().unwrap(), 1, 3, 5));
-    println!("part 1: {}", run(&mut stacks.clone(), cmds.clone(), false));
-    println!("part 2: {}", run(&mut stacks, cmds, true));
+    let (init, code) = input.split("\n\n").collect_tuple().unwrap();
+    let mut stacks = parse_stacks(init);
+    println!("part 1: {}", run(&mut stacks.clone(), code, true));
+    println!("part 2: {}", run(&mut stacks, code, false));
 }
