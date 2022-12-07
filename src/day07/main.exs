7e5fe0ade28912affadb08cc88574cfbf2520c30
Mike Meyer <mike@meyermike.com>
2022-12-07 16:10:46 -0500

    day7 elixir

--- src/day07/main.exs
+++ src/day07/main.exs
@@ -1,46 +1,54 @@
 defmodule Day07 do
-  def all_uniq(s) do
-    String.graphemes(s)
-    |> Enum.uniq()
-    |> Enum.count() ==
-      String.length(s)
+  def process_line(line, {cd, sizes}) do
+    case String.split(line, ~r/\s+/, trim: true) do
+      ["$", "cd", ".."] ->
+        {tl(cd), sizes}
+
+      ["$", "cd", "/"] ->
+        {[], sizes}
+
+      ["$", "cd", name] ->
+        {[name | cd], sizes}
+
+      ["$", "ls"] ->
+        {cd, sizes}
+
+      ["dir", name] ->
+        {cd, Map.put(sizes, [name | cd], 0)}
+
+      [s, _] ->
+        {bytes, _} = Integer.parse(s)
+        {cd, Day07.updateSizes(cd, bytes, sizes)}
+    end
   end
 
-  def detect(input, n) do
-    {_, i} =
-      Day06.windows(input, n)
-      |> Stream.with_index()
-      |> Stream.filter(fn {s, _} ->
-        Day06.all_uniq(s)
-      end)
-      |> Enum.at(0)
-
-    i + n
-  end
-
-  def cmd({cd, sizes}, tokens) do
-    case tokens do
-      ["cd", ".."] -> {tl(cd), sizes}
-      ["cd", "/"] -> {[], sizes}
-      ["cd", name] -> {[name|cd], sizes}
-      ["ls"] -> {cd, sizes}
-      [s, _] when String.match?(s, ~r/\d*/) ->
+  def updateSizes(path, bytes, sizes) do
+    if Enum.empty?(path) do
+      Map.update!(sizes, path, &(&1 + bytes))
+    else
+      updateSizes(tl(path), bytes, Map.update!(sizes, path, &(&1 + bytes)))
     end
   end
 end
 
-input =
+{_, data} =
   File.read!("puzzle_input/day07")
-  |> String.split(~r/\R/, trim: True)
-  |> Enum.reduce({[], %{}}, fn line, state ->
-    case String.split(line, ~r/\s/) do
-      ["$"|rest] -> Day07.cmd(state, rest)
-      ["dir", name] ->
-        {cd, sizes} = state
-        {cd, Map.put(sizes, name, 0)}
-    end
-  end)
-  |> IO.inspect()
+  |> String.trim_trailing()
+  |> String.split(~r/\R+/, trim: True)
+  |> Enum.reduce({[], %{[] => 0}}, &Day07.process_line/2)
 
-IO.puts("part 1: #{0}")
-IO.puts("part 2: #{0}")
+p1 =
+  Map.values(data)
+  |> Enum.filter(&(&1 < 100_000))
+  |> Enum.sum()
+
+IO.puts("part 1: #{p1}")
+
+toFree = 30_000_000 - 70_000_000 + Map.get(data, [])
+
+p2 =
+  Map.values(data)
+  |> Enum.filter(&(&1 >= toFree))
+  |> Enum.min()
+
+IO.puts("part 2: #{p2}")
