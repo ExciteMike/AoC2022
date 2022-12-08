896c9dd4bf12b6aefa4cf3e96387f7e76eef5e5c
ExciteMike <mike@meyermike.com>
2022-12-07 19:03:27 -0500

    terser rust day 7

--- src/day07/main.rs
+++ src/day07/main.rs
@@ -4,74 +4,46 @@ use std::collections::HashMap;
 use itertools::Itertools;
 use shared::puzzle_input;
 
-struct State {
-    sizes: HashMap<Vec<String>, usize>,
-    space: usize,
-    cd: Vec<String>,
-}
-
 const TARGET: usize = 30000000;
 
-fn cd<'a, I: Iterator<Item = &'a str>>(state: &mut State, mut tokens: I) {
-    match tokens.next().unwrap() {
-        ".." => {
-            state.cd.pop();
-        }
-        "/" => {
-            state.cd.clear();
-        }
-        s => state.cd.push(s.to_owned()),
-    }
-}
-
-fn cmd<'a, I: Iterator<Item = &'a str>>(state: &mut State, mut tokens: I) {
-    if "cd" == tokens.next().unwrap() {
-        cd(state, tokens)
-    }
-}
-
-fn dir<'a, I: Iterator<Item = &'a str>>(state: &mut State, mut tokens: I) {
-    let mut path = state.cd.clone();
-    path.push(tokens.next().unwrap().to_owned());
-    state.sizes.insert(path, 0);
-}
-
-fn size(state: &mut State, file_size: usize) {
-    let mut path = state.cd.clone();
-    while !path.is_empty() {
-        let x = state.sizes.get_mut(&path).unwrap();
-        *x += file_size;
-        path.pop();
-    }
-
-    state.space -= file_size;
-}
-
 pub fn main() {
-    let mut state = State {
-        sizes: HashMap::from([(vec![String::from("/")], 0)]),
-        space: 70000000,
-        cd: vec![],
-    };
+    let mut sizes = HashMap::from([(vec!["/"], 0)]);
+    let mut space = 70000000;
+    let mut cd = vec![];
     let input = puzzle_input!();
     for line in input.split('\n') {
-        let mut tokens = line.split_whitespace();
-        match tokens.next().unwrap() {
-            "$" => cmd(&mut state, tokens),
-            "dir" => dir(&mut state, tokens),
-            s => size(&mut state, s.parse().unwrap()),
+        let v = line.split_whitespace().collect_vec();
+        match v.as_slice() {
+            ["$", "cd", ".."] => {
+                cd.pop();
+            }
+            ["$", "cd", "/"] => cd.clear(),
+            ["$", "cd", s] => cd.push(*s),
+            ["$", "ls"] => (),
+            ["dir", s] => {
+                let mut path = cd.clone();
+                path.push(s);
+                sizes.insert(path, 0);
+            }
+            [s, _] if s.chars().all(|c| c.is_digit(10)) => {
+                let file_size: usize = s.parse().unwrap();
+                let mut path = cd.clone();
+                while !path.is_empty() {
+                    let x = sizes.get_mut(&path).unwrap();
+                    *x += file_size;
+                    path.pop();
+                }
+
+                space -= file_size;
+            }
+            _ => panic!(),
         }
     }
 
-    let p1: usize = state.sizes.values().filter(|&&x| x <= 100000).sum();
+    let p1: usize = sizes.values().filter(|&&x| x <= 100000).sum();
 
-    let needed = TARGET - state.space;
-    let p2 = state
-        .sizes
-        .values()
-        .filter(|&&x| x >= needed)
-        .min()
-        .unwrap();
+    let needed = TARGET - space;
+    let p2 = sizes.values().filter(|&&x| x >= needed).min().unwrap();
 
     println!("part 1: {}\npart 2: {}", p1, p2);
 }
